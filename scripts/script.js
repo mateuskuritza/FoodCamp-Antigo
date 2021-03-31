c50b000f8bae4244abfe5b0163ccfbfe81b5f42b
mateuskuritza <mateuskuritza@gmail.com>
2021-03-31 12:35:43 -0300

    js somar valores

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
 
-
-
 function prato_selecionado(element) {
     const selecionados = document.getElementsByClassName("prato-selecionado");
 
     if( selecionados.length > 0 ){
         selecionados[0].classList.remove("prato-selecionado");
     }
-    
+
     element.classList.add("prato-selecionado");
+
+    const valorpedido = element.getElementsByClassName("valor-pedido");
+    valor_prato = Number(valorpedido[0].innerHTML.replace("R$ ", ''));
 }
 
 function bebida_selecionada(element) {
@@ -19,6 +20,9 @@ function bebida_selecionada(element) {
     }
     
     element.classList.add("bebida-selecionada");
+
+    const valorpedido = element.getElementsByClassName("valor-pedido");
+    valor_bebida = Number(valorpedido[0].innerHTML.replace("R$ ", ''));
 }
 
 function sobremesa_selecionada(element) {
@@ -29,4 +33,14 @@ function sobremesa_selecionada(element) {
     }
     
     element.classList.add("sobremesa-selecionada");
+
+    const valorpedido = element.getElementsByClassName("valor-pedido");
+    valor_sobremesa = Number(valorpedido[0].innerHTML.replace("R$ ", ''));
+}
+
+function calcular_total(){
+
+    total = valor_prato + valor_bebida + valor_sobremesa;
+    console.log(total);
+    
 }
