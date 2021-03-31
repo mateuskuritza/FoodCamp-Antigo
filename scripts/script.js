8a95493f579b03121512fec1c6e8a61c28a2a993
mateuskuritza <mateuskuritza@gmail.com>
2021-03-31 17:51:35 -0300

    querySelector

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,54 @@
 
 function prato_selecionado(element) {
-    const selecionados = document.getElementsByClassName("prato-selecionado");
-
-    if( selecionados.length > 0 ){
-        selecionados[0].classList.remove("prato-selecionado");
+    const selecionados = document.querySelector(".prato-selecionado");
+    
+    if( selecionados !== null ){
+        selecionados.classList.remove("prato-selecionado");
     }
 
     element.classList.add("prato-selecionado");
 
-    const valorpedido = element.getElementsByClassName("valor-pedido");
-    valor_prato = Number(valorpedido[0].innerHTML.replace("R$ ", ''));
+    const valorpedido = element.querySelector(".valor-pedido");
+    valor_prato = Number(valorpedido.innerHTML.replace("R$ ", ''));
 
-    pratopedido = element.getElementsByClassName("nome-pedido");
-    pratopedido = pratopedido[0].innerHTML;
+    pratopedido = element.querySelector(".nome-pedido");
+    pratopedido = pratopedido.innerHTML;
 
     testarselecao();
 }
 
 function bebida_selecionada(element) {
-    const selecionados = document.getElementsByClassName("bebida-selecionada");
+    const selecionados = document.querySelector(".bebida-selecionada");
 
-    if( selecionados.length > 0 ){
-        selecionados[0].classList.remove("bebida-selecionada");
+    if( selecionados !== null ){
+        selecionados.classList.remove("bebida-selecionada");
     }
     
     element.classList.add("bebida-selecionada");
 
-    const valorpedido = element.getElementsByClassName("valor-pedido");
-    valor_bebida = Number(valorpedido[0].innerHTML.replace("R$ ", ''));
+    const valorpedido = element.querySelector(".valor-pedido");
+    valor_bebida = Number(valorpedido.innerHTML.replace("R$ ", ''));
 
-    bebidapedido = element.getElementsByClassName("nome-pedido");
-    bebidapedido = bebidapedido[0].innerHTML;
+    bebidapedido = element.querySelector(".nome-pedido");
+    bebidapedido = bebidapedido.innerHTML;
 
     testarselecao();
 }
 
 function sobremesa_selecionada(element) {
-    const selecionados = document.getElementsByClassName("sobremesa-selecionada");
+    const selecionados = document.querySelector(".sobremesa-selecionada");
 
-    if( selecionados.length > 0 ){
-        selecionados[0].classList.remove("sobremesa-selecionada");
+    if( selecionados !== null ){
+        selecionados.classList.remove("sobremesa-selecionada");
     }
     
     element.classList.add("sobremesa-selecionada");
 
-    const valorpedido = element.getElementsByClassName("valor-pedido");
-    valor_sobremesa = Number(valorpedido[0].innerHTML.replace("R$ ", ''));
+    const valorpedido = element.querySelector(".valor-pedido");
+    valor_sobremesa = Number(valorpedido.innerHTML.replace("R$ ", ''));
 
-    sobremesapedido = element.getElementsByClassName("nome-pedido");
-    sobremesapedido = sobremesapedido[0].innerHTML;
+    sobremesapedido = element.querySelector(".nome-pedido");
+    sobremesapedido = sobremesapedido.innerHTML;
 
     testarselecao();
 }
@@ -59,8 +59,8 @@ function calcular_total(){
 
 function testarselecao(){
     if(valor_prato*valor_bebida*valor_sobremesa!=0){
-        const botaohabilitado = document.getElementById("botao-habilitado");
-        const botaodesabilitado = document.getElementById("botao-desabilitado");
+        const botaohabilitado = document.querySelector("#botao-habilitado");
+        const botaodesabilitado = document.querySelector("#botao-desabilitado");
         botaohabilitado.style.display = "inherit";
         botaodesabilitado.style.display = "none";
         calcular_total();
@@ -68,11 +68,6 @@ function testarselecao(){
 }
 
 function mensagemwhats(){
-    pratopedido
-    bebidapedido
-    sobremesapedido
-    total
-
     const mensagem = "https://api.whatsapp.com/send?phone=5542998043116&text="+encodeURIComponent("Olá, gostaria de fazer o pedido: - Prato: " + pratopedido + " - Bebida: " + bebidapedido + " - Sobremesa: " + sobremesapedido + " Total: R$ " + total);
-    window.location.href = mensagem;
+    window.open(mensagem);
 }
