a17e3f53e3dfbddd245537b427f455dc73300960
mateuskuritza <mateuskuritza@gmail.com>
2021-03-31 11:51:24 -0300

    js function select

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,32 @@
-function pratoselecionado(element) {
 
-    element.classList.add("pratoselecionado");
+
+
+function prato_selecionado(element) {
+    const selecionados = document.getElementsByClassName("prato-selecionado");
+
+    if( selecionados.length > 0 ){
+        selecionados[0].classList.remove("prato-selecionado");
+    }
+    
+    element.classList.add("prato-selecionado");
+}
+
+function bebida_selecionada(element) {
+    const selecionados = document.getElementsByClassName("bebida-selecionada");
+
+    if( selecionados.length > 0 ){
+        selecionados[0].classList.remove("bebida-selecionada");
+    }
+    
+    element.classList.add("bebida-selecionada");
+}
+
+function sobremesa_selecionada(element) {
+    const selecionados = document.getElementsByClassName("sobremesa-selecionada");
+
+    if( selecionados.length > 0 ){
+        selecionados[0].classList.remove("sobremesa-selecionada");
+    }
+    
+    element.classList.add("sobremesa-selecionada");
 }
